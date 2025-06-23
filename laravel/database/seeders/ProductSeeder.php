a652e3026ee35c2fd0922c4907d5005d18c8c458
kajihara Ai <ai.kajihara.tr@ambl.co.jp>
2025-06-23 14:43:04 +0900

    Category,Product,Userのダミーデータ数件だけ作成

--- laravel/database/seeders/ProductSeeder.php
+++ laravel/database/seeders/ProductSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +14,31 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('products')->insert([
+            [
+                'name' => 'レトロ本革カウチソファ',
+                'price' => 79800,
+                'image_path' => 'images/products/leather_couch.jpg',
+                'description' => '上質な本革を使用したレトロデザインのカウチソファ。3人掛けで、座り心地も抜群。',
+                'stock' => 15,
+                'category_id' => 1,
+            ],
+            [
+                'name' => '引き出し付きシングルベッド',
+                'price' => 39900,
+                'image_path' => 'images/products/single_bed_drawer.jpg',
+                'description' => '限られたスペースを有効活用できる、引き出し付きのシングルベッド。衣類や寝具の収納に便利です。',
+                'stock' => 20,
+                'category_id' => 2,
+            ],
+            [
+                'name' => '北欧柄コットンラグ',
+                'price' => 2999, 
+                'image_path' => 'images/products/nordic_cotton_rug.jpg',
+                'description' => '北欧スタイルの幾何学模様が特徴のコットンラグ。軽量で洗濯可能、ナチュラルな質感がリビングやベッドルームにぴったり。',
+                'stock' => 30,
+                'category_id' => 3, 
+            ],
+        ]);
     }
 }
