50808a41e589315b78b668e81d559e28156b3ef3
ogata <shunya.ogata.hv@ambl.co.jp>
2025-06-24 14:11:55 +0900

    テスト用route

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -9,16 +9,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-
 Route::post('/register', [AuthController::class, 'register']);   // ★任意
-Route::post('/login',    [AuthController::class, 'login'])->name('api.login');
+Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 
 // Route::apiResource('products', ProductController::class);
 
 // auth:sanctumで囲むとトークン必要(ログイン必要)
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('products', ProductController::class);
+    //Route::apiResource('admin/products', ProductController::class);
 });
 
+//テスト用
+Route::apiResource('admin/products', ProductController::class);
