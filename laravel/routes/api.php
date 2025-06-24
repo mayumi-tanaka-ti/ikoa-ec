8264533ebb715489a553bed9efdb3de1b6cf9f85
Ryo Egawa <ryo.egawa.to@ambl.co.jp>
2025-06-24 13:49:12 +0900

    ユーザー側のルート、コントローラー

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('products', ProductController::class)->only(['index','show']);
+Route::get('/products/list', [ProductController::class, 'list']);
