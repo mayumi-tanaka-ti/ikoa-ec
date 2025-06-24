035204838a0f634650f16ccbe541a1135c477608
mayumi <mayumi.tanaka.vr@ambl.com>
2025-06-24 13:43:40 +0900

    gate編集しました

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::define('admin', function ($user) {
-            return $user->role >= 5;
+            return $user->is_admin = true;
         });
     }
 
