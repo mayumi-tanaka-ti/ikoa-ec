b0ef9fc09a7f7c27925f377a51e396a2d319698d
mayumi <mayumi.tanaka.vr@ambl.com>
2025-06-25 14:42:09 +0900

    history修正

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -49,7 +49,7 @@ Route::apiResource('cart', CartController::class);
     Route::middleware(['auth:sanctum','can:admin'])->group(function () {
     Route::apiResource('admin/products', ProductController::class);
     Route::apiResource('admin/categories', CategoryController::class);
-    Route::apiResource('admin/history', AdminController::class);
+    Route::get('admin/history/{id}', [AdminController::class,'history']);
     Route::apiResource('admin/users', AdminController::class);
     Route::apiResource('admin/orders', OrderController::class);
 });
