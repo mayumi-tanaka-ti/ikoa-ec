fb6d40188ce0f63f5412b117719432095de56706
Ryo Egawa <ryo.egawa.to@ambl.co.jp>
2025-06-26 11:53:40 +0900

    reviewのルート、コントローラー、商品詳細へのレビューの実装

--- laravel/app/Http/Controllers/Middleware/Authenticate.php
+++ laravel/app/Http/Controllers/Middleware/Authenticate.php
@@ -9,9 +9,9 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         // expectsJsonの場合はリダイレクトせず401を返す
-        if (! $request->expectsJson()) {
-            return route('api.login');
-        }
+         if (! $request->expectsJson()) {
+              return route('api.login');
+          }
         // expectsJsonの場合はnullを返す（リダイレクトしない）
         return null;
     }
